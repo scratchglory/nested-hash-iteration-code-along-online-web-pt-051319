fe8b8859bb669ad2f971d69cf0f56d71e9c4a207
Adella <adellakenn@gmail.com>
2019-05-16 13:11:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
+def remove_strawberry
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
